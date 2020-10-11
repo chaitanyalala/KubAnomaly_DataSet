936c26b8f0b6bd597f9acf9ec1294e952f893a45
Chaitanya Lala <chaitanya.lala@gmail.com>
2020-10-10 22:24:06 -0700

    Add run option to script

--- KubAnomalyAgent/compile.sh
+++ KubAnomalyAgent/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 rm -fr build
 gradle -b build.gradle fatJar
-ls -l build/libs/DAVIDAgent-0.0.1.jar
+JAR=build/libs/DAVIDAgent-0.0.1.jar
+ls -l $JAR
+if [ ! -e $JAR ]; then
+	echo "Compilation Failed"
+	exit 1
+fi
+
+java -jar $JAR
+
